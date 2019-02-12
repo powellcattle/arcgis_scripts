eeea75bf5efba573bdb36846339463631f757eb7
powellcattle <43384789+powellcattle@users.noreply.github.com>
2019-02-12 13:47:12 -0600

    Changes to logic

--- geodatabase/versionalldatasets.py
+++ geodatabase/versionalldatasets.py
@@ -1,28 +1,26 @@
 __author__ = "spowell"
 import arcpy
 
+work_space = arcpy.env["scriptWorkspace"]
+
 try:
-    data_set = arcpy.GetParameterAsText(0)
-    is_compress_db = False
-    if arcpy.GetArgumentCount() > 1:
-        is_compress_db = arcpy.GetParameterAsText(1)
-    work_space = arcpy.env["scriptWorkspace"]
+
     arcpy.env.workspace = work_space
     assert work_space
 
+    # Don't allow any more connections to the geodatabase
+    # and disconnect existing users
     arcpy.AddMessage(f"Disallowing new connections.")
     arcpy.AcceptConnections(work_space, False)
     arcpy.AddMessage(f"Disconnecting all Users.")
+    arcpy.DisconnectUser(work_space, "ALL")
 
-    datasets = arcpy.ListDatasets("", r"Feature")
-    for data_set in datasets:
-        # if data_set == "ec.sde.WorkOrders":
-        #     continue
+    data_sets = arcpy.ListDatasets("", r"Feature")
+    for data_set in data_sets:
         data_set_desc = arcpy.Describe(data_set)
         if not data_set_desc.isVersioned:
-            arcpy.DisconnectUser(work_space, "ALL")
-            arcpy.AddMessage(f"Registering {data_set} as Versioned.")
             try:
+                arcpy.AddMessage(f"Registering {data_set} as Versioned.")
                 arcpy.RegisterAsVersioned_management(data_set, "EDITS_TO_BASE")
             except Exception as e:
                 arcpy.AddError(f"Could not Version.\n {data_set} {data_set_desc}\n{arcpy.GetMessages(2)}")
@@ -39,4 +37,5 @@ except Exception as e:
 finally:
     arcpy.AddMessage(f"Accepting User Connections.")
     arcpy.AcceptConnections(work_space, True)
+
 
